20c1a5bc22f2d27d10c4d68a244aeaa6558ff150
mihajlo182savic <savic.ra182.2019@uns.ac.rs>
2022-10-26 23:08:21 +0200

    Http request port changed, minor bugfixes on feedback

--- src/app/modules/feedback/services/feedback.service.ts
+++ src/app/modules/feedback/services/feedback.service.ts
@@ -8,28 +8,28 @@ import { FeedBack } from '../model/feedback.model';
 })
 export class FeedbackService {
 
-  apiHost: string = 'http://localhost:16177/';
+  apiHost: string = 'http://localhost:5000/';
   headers: HttpHeaders = new HttpHeaders({ 'Content-Type': 'application/json' });
 
   constructor(private http: HttpClient) { }
 
   getFeedbacks(): Observable<FeedBack[]> {
-    return this.http.get<FeedBack[]>(this.apiHost + 'api/feedbacks', {headers: this.headers});
+    return this.http.get<FeedBack[]>(this.apiHost + 'api/intranet/feedbacks', {headers: this.headers});
   }
   getFeedback(id: number): Observable<FeedBack> {
-    return this.http.get<FeedBack>(this.apiHost + 'api/feedbacks/' + id, {headers: this.headers});
+    return this.http.get<FeedBack>(this.apiHost + 'api/intranet/feedbacks/' + id, {headers: this.headers});
   }
 
   deleteFeedback(id: any): Observable<any> {
-    return this.http.delete<any>(this.apiHost + 'api/feedbacks/' + id, {headers: this.headers});
+    return this.http.delete<any>(this.apiHost + 'api/intranet/feedbacks/' + id, {headers: this.headers});
   }
 
   createFeedback(feedback: any): Observable<any> {
-    return this.http.post<any>(this.apiHost + 'api/feedbacks', feedback, {headers: this.headers});
+    return this.http.post<any>(this.apiHost + 'api/intranet/feedbacks', feedback, {headers: this.headers});
   }
 
   updateFeedback(feedback: any): Observable<any> {
-    return this.http.put<any>(this.apiHost + 'api/feedbacks/' + feedback.id, feedback, {headers: this.headers});
+    return this.http.put<any>(this.apiHost + 'api/intranet/feedbacks/' + feedback.id, feedback, {headers: this.headers});
   }
 
   
