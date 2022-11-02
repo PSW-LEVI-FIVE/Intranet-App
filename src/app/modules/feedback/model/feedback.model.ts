c4e39db620700e2bfee0de1da0c715d78ad29ce4
mihajlo182savic <savic.ra182.2019@uns.ac.rs>
2022-11-02 17:04:40 +0100

    Rebase commit

--- src/app/modules/feedback/model/feedback.model.ts
+++ src/app/modules/feedback/model/feedback.model.ts
@@ -1,8 +1,16 @@
 export interface IFeedBack {
+<<<<<<< HEAD
     id?: number;
     patientId: number;
     feedbackContent: string;
     allowPublishment : boolean;
     published: boolean;
     anonimity: boolean;
+=======
+    id: number;
+    patient: string;
+    patientId: number;
+    feedbackContent: string; 
+
+>>>>>>> 93712d7 (Catch patient name and surname from backend)
 }
