821f9fc47ed57e66ef176d391f18dbc07f938103
Lee <licina.luka@gmail.com>
2022-11-03 21:06:03 +0100

    fix

--- src/app/doctor/appointments/view-form/view-form.component.ts
+++ src/app/doctor/appointments/view-form/view-form.component.ts
@@ -10,7 +10,7 @@ import {IAppointment} from "../create-form/create-form.component";
 })
 export class ViewFormComponent implements OnInit {
 
-  public title:string ='Update Appointment';
+  public title:string ='View Appointment';
   public error:string = '';
   public roomName:string='';
   public patientName:string='';
