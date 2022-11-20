030d861127bcbd7f1145b88900cd762b2e1ed22d
mihajlo182savic <savic.ra182.2019@uns.ac.rs>
2022-11-20 01:27:15 +0100

    Fixed bug

--- src/app/shared/login/service/login.service.ts
+++ src/app/shared/login/service/login.service.ts
@@ -14,12 +14,12 @@ export class LoginService {
   constructor(private http: HttpClient) { }
 
   makeLogin(login: any): Observable<any> {
-    return this.http.post(this.apiHost + 'api/public/login/login', login, {headers: this.headers,responseType:'text'});
+    return this.http.post(this.apiHost + 'api/intranet/login/login', login, {headers: this.headers,responseType:'text'});
   }
 
   getUserProfile()
   {
     var TokenHeader = new HttpHeaders({'Authorization':'Bearer ' + localStorage.getItem('token')});
-    return this.http.get(this.apiHost + 'api/public/login/user',{headers:TokenHeader,responseType:'text'});
+    return this.http.get(this.apiHost + 'api/intranet/login/user',{headers:TokenHeader,responseType:'text'});
   }
 }
