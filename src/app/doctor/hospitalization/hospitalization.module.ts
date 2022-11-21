c0e5f925f4c8384b115c56e110897aebd6ad2a3d
DavidT <terenji.ra178.2019@uns.ac.rs>
2022-11-21 22:41:18 +0100

    merging conflicts resolved

--- src/app/doctor/hospitalization/hospitalization.module.ts
+++ src/app/doctor/hospitalization/hospitalization.module.ts
@@ -7,20 +7,10 @@ import { MaterialModule } from 'src/app/material/material.module';
 import { MedicalRecordHeaderComponent } from './medical-record-view/components/medical-record-header/medical-record-header.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from "../../shared/shared.module";
-<<<<<<< HEAD
 import { CreateHospitalizationFormComponent } from './medical-record-view/components/create-hospitalization-form/create-hospitalization-form.component';
 import { GiveTherapyFormComponent } from './give-therapy-form/give-therapy-form.component';
 import { GiveBloodTherapyComponent } from './give-therapy-form/components/give-blood-therapy/give-blood-therapy.component';
 import { GiveMedicineTherapyComponent } from './give-therapy-form/components/give-medicine-therapy/give-medicine-therapy.component';
-=======
-import { GiveTherapyFormComponent } from './give-therapy-form/give-therapy-form.component';
-import { GiveBloodTherapyComponent } from './give-therapy-form/components/give-blood-therapy/give-blood-therapy.component';
-import { GiveMedicineTherapyComponent } from './give-therapy-form/components/give-medicine-therapy/give-medicine-therapy.component';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
->>>>>>> 7230d1fc355eee26a94e89a85ddf1469b623d476
-
 
 
 @NgModule({
@@ -28,20 +18,11 @@ import { MatSelectModule } from '@angular/material/select';
     HospitalizationComponent,
     MedicalRecordViewComponent,
     MedicalRecordHeaderComponent,
-<<<<<<< HEAD
     CreateHospitalizationFormComponent,
-=======
->>>>>>> 7230d1fc355eee26a94e89a85ddf1469b623d476
     GiveTherapyFormComponent,
     GiveBloodTherapyComponent,
     GiveMedicineTherapyComponent,
-    GiveBloodTherapyComponent,
-    GiveMedicineTherapyComponent,
     GiveTherapyFormComponent,
-<<<<<<< HEAD
-=======
-
->>>>>>> 7230d1fc355eee26a94e89a85ddf1469b623d476
   ],
   exports: [RouterModule,
   ],
@@ -50,15 +31,8 @@ import { MatSelectModule } from '@angular/material/select';
     RouterModule,
     MaterialModule,
     ReactiveFormsModule,
-<<<<<<< HEAD
     FormsModule,
     SharedModule,
-=======
-    SharedModule,
-    MatInputModule,
-    MatFormFieldModule,
-    MatSelectModule,
->>>>>>> 7230d1fc355eee26a94e89a85ddf1469b623d476
   ]
 })
 export class HospitalizationModule { }
