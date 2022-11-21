f122f023bde2dafb7550f7ee4426476f38b46b0d
DavidT <terenji.ra178.2019@uns.ac.rs>
2022-11-21 21:10:51 +0100

    prescribing therapy form made

--- src/app/doctor/hospitalization/hospitalization.module.ts
+++ src/app/doctor/hospitalization/hospitalization.module.ts
@@ -7,10 +7,19 @@ import { MaterialModule } from 'src/app/material/material.module';
 import { MedicalRecordHeaderComponent } from './medical-record-view/components/medical-record-header/medical-record-header.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from "../../shared/shared.module";
+<<<<<<< HEAD
 import { CreateHospitalizationFormComponent } from './medical-record-view/components/create-hospitalization-form/create-hospitalization-form.component';
 import { GiveTherapyFormComponent } from './give-therapy-form/give-therapy-form.component';
 import { GiveBloodTherapyComponent } from './give-therapy-form/components/give-blood-therapy/give-blood-therapy.component';
 import { GiveMedicineTherapyComponent } from './give-therapy-form/components/give-medicine-therapy/give-medicine-therapy.component';
+=======
+import { GiveTherapyFormComponent } from './give-therapy-form/give-therapy-form.component';
+import { GiveBloodTherapyComponent } from './give-therapy-form/components/give-blood-therapy/give-blood-therapy.component';
+import { GiveMedicineTherapyComponent } from './give-therapy-form/components/give-medicine-therapy/give-medicine-therapy.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+>>>>>>> prescribing therapy form made
 
 
 
@@ -19,13 +28,20 @@ import { GiveMedicineTherapyComponent } from './give-therapy-form/components/giv
     HospitalizationComponent,
     MedicalRecordViewComponent,
     MedicalRecordHeaderComponent,
+<<<<<<< HEAD
     CreateHospitalizationFormComponent,
+=======
+>>>>>>> prescribing therapy form made
     GiveTherapyFormComponent,
     GiveBloodTherapyComponent,
     GiveMedicineTherapyComponent,
     GiveBloodTherapyComponent,
     GiveMedicineTherapyComponent,
     GiveTherapyFormComponent,
+<<<<<<< HEAD
+=======
+
+>>>>>>> prescribing therapy form made
   ],
   exports: [RouterModule,
   ],
@@ -34,8 +50,15 @@ import { GiveMedicineTherapyComponent } from './give-therapy-form/components/giv
     RouterModule,
     MaterialModule,
     ReactiveFormsModule,
+<<<<<<< HEAD
     FormsModule,
     SharedModule,
+=======
+    SharedModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSelectModule,
+>>>>>>> prescribing therapy form made
   ]
 })
 export class HospitalizationModule { }
