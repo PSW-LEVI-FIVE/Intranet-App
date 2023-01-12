abc8b25dc35fddb867f99fb6b049961ca160011b
Kristian Krizzan <krizan.ra196.2019@uns.ac.rs>
2023-01-12 20:37:34 +0100

    done statistics for scheduling

---
+++ src/app/manager/scheduling-appointment-statistics/scheduling-appointment-statistics.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SchedulingAppointmentStatisticsComponent } from './scheduling-appointment-statistics.component';
+import { NgChartsModule } from 'ng2-charts';
+import { SharedModule } from "../../shared/shared.module";
+import { FormsModule } from '@angular/forms'
+
+
+
+@NgModule({
+    declarations: [
+        SchedulingAppointmentStatisticsComponent
+    ],
+    imports: [
+        CommonModule,
+        NgChartsModule,
+        SharedModule,
+        FormsModule
+    ]
+})
+export class SchedulingAppointmentStatisticsModule { }
